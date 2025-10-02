4c27f9235ffb65545a131368153dbbd7e70545a4
EehMauro <maurojoaquinescudero@gmail.com>
2025-10-02 11:03:41 -0300

    updated

--- main.ts
+++ main.ts
@@ -1,10 +1,25 @@
 import fs from 'fs';
-import { Parser, AstCompiler, defaultUplcVersion } from '@harmoniclabs/pebble';
+import { Parser, Compiler, defaultUplcVersion } from '@harmoniclabs/pebble';
+
+// import Parser from 'tree-sitter';
+// const Pebble = require('tree-sitter-pebble');
+
+// const parser = new Parser();
+// parser.setLanguage(Pebble);
+
+const FILE = 'examples/test.pebble';
 
 async function main() {
-  const result = fs.readFileSync('main.pebble', 'utf8');
-  const [source, diagnostics] = Parser.parseFile('main.pebble', result);
+  const result = fs.readFileSync(FILE, 'utf8');
+  
+  // const tree = parser.parse(result);
+  
+  // console.log('-- TREE --');
+  // console.log(tree.rootNode);
+  // console.log(tree.rootNode.children);
 
+  const [source, diagnostics] = Parser.parseFile(FILE, result);
+  
   // console.log('-- SOURCE --');
   // console.log(source);
 
@@ -12,18 +27,7 @@ async function main() {
   // console.log(diagnostics);
 
   // console.log('-- COMPILER --');
-  
-  const compiler = new AstCompiler(
-    {
-      root: '.',
-      entry: 'main.pebble',
-      outDir: 'dist',
-      targetUplcVersion: defaultUplcVersion,
-      removeTraces: false,
-      delayHoists: false,
-      uplcOptimizations: {},
-      addMarker: false,
-    },
+  const compiler = new Compiler(
     {
       stdout: process.stdout,
       stderr: process.stderr,
@@ -32,6 +36,16 @@ async function main() {
       exsistSync: (filename: string) => fs.existsSync(filename),
       listFiles: (dirname: string, baseDir: string) => fs.readdirSync(`${baseDir}/${dirname}`),
       reportDiagnostic: () => {},
+    },
+    {
+      root: '.',
+      entry: FILE,
+      outDir: 'dist',
+      targetUplcVersion: defaultUplcVersion,
+      removeTraces: false,
+      delayHoists: false,
+      uplcOptimizations: {},
+      addMarker: false,
     },
     diagnostics
   );
